ec8e1ba51167fe2f87e34877ecf47e2c3ecd83d1
GingerRin <youarenottakingthisseriously@gmail.com>
2019-11-25 16:12:17 +0800

    Cat Food!
    Updated:
    index.html
    main.js
    
    -Added Cat Food as upgrade
    -Generally stylized

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var gameData = {
   click: 0,
+  highestClick: 0,
   clickPerClick: 1,
   costPerClick: 10,
   gainPerClick: 1,
@@ -9,7 +10,10 @@ var gameData = {
   gainPerCat: 1,
   costScale2: 50,
   clickPerCat: 0,
-  gainPerCatClick: 1
+  gainPerCatClick: 1,
+  catFood: 0,
+  costCatFood: 500,
+  boughtCatFood: false
 }
 
 function Update(){
@@ -18,18 +22,34 @@ function Update(){
 }
 
 function Main(){
-	if(gameData.click>=10){
+	if(gameData.click>=gameData.highestClick) gameData.highestClick = gameData.click
+	if(gameData.highestClick>=10){
 		document.getElementById("buyClick").style.visibility = 'visible'
 		document.getElementById("currCostPerClick").style.visibility = 'visible'
 	}
-	if(gameData.click>=100){
+
+	if(gameData.click<=gameData.costPerClick) document.getElementById("buttonCPC").disabled = true
+	else document.getElementById("buttonCPC").disabled = false
+	
+	if(gameData.highestClick>=100){
 		document.getElementById("buyCat").style.visibility = 'visible'
 		document.getElementById("currCostPerCat").style.visibility = 'visible'
 	}
+	
+	if(gameData.click<=gameData.costPerCat) document.getElementById("buttonCat").disabled = true
+	else document.getElementById("buttonCat").disabled = false
+	
+	if(gameData.highestClick>=500 && gameData.boughtCatFood == false && gameData.cat>=1){
+		document.getElementById("buyCatFood").style.visibility = 'visible'
+		document.getElementById("currCostCatFood").style.visibility = 'visible'
+		document.getElementById("descCatFood").style.visibility = 'visible'
+	}
+	if(gameData.click<=gameData.costCatFood) document.getElementById("buttonCatFood").disabled = true
+	else document.getElementById("buttonCatFood").disabled = false
 }
 
 function catClick(){
-	gameData.click += gameData.clickPerCat
+	gameData.click += gameData.clickPerCat * gameData.gainPerCatClick
 	document.getElementById("buttonClicked").innerHTML = gameData.click + " clicks"
 }
 
@@ -56,7 +76,7 @@ function buyCat(){
 		gameData.cat += gameData.gainPerCat
 		gameData.costPerCat += gameData.costScale2
 		gameData.costScale2 += gameData.costScale2
-		gameData.clickPerCat += gameData.gainPerCatClick
+		gameData.clickPerCat += 1
 		document.getElementById("currCat").style.visibility = 'visible'
 		if(gameData.cat<=1){
 			document.getElementById("currCat").innerHTML = "You have " + gameData.cat + " cat"
@@ -67,6 +87,19 @@ function buyCat(){
 		document.getElementById("currCostPerCat").innerHTML = "Cost: " + gameData.costPerCat + " clicks"
 		document.getElementById("currCat2").style.visibility = 'visible'
 		document.getElementById("currClickPerCat").style.visibility = 'visible'
-		document.getElementById("currClickPerCat").innerHTML = gameData.clickPerCat + " CpS"
+		document.getElementById("currClickPerCat").innerHTML = gameData.clickPerCat * gameData.gainPerCatClick + " CpS"
+	}
+}
+
+function buyCatFood(){
+	if(gameData.click>=gameData.costCatFood){
+		gameData.click -= gameData.costCatFood
+		gameData.gainPerCatClick *= 2 
+		gameData.buyCatFood = true
+		document.getElementById("buttonClicked").innerHTML = gameData.click + " clicks"
+		document.getElementById("currClickPerCat").innerHTML = gameData.clickPerCat * gameData.gainPerCatClick + " CpS"
+		document.getElementById("buttonCatFood").innerHTML = "Bought cat food"
+		document.getElementById("currCostCatFood").innerHTML = ""
+		document.getElementById("descCatFood").innerHTML = "Your cats finally have something to eat"
 	}
 }
